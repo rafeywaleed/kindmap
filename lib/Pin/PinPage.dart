de427c350b956bbfe86b2fc24542321c4228e759
rafeywaleed <a.rafeywaleeda5@gmail.com>
2024-06-03 00:59:23 +0530

    Homepage changes

--- lib/Pin/PinPage.dart
+++ lib/Pin/PinPage.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutterflow_ui/flutterflow_ui.dart';
@@ -186,61 +187,115 @@ class _PinPageState extends State<PinPage> with TickerProviderStateMixin {
   //   }
   // }
 
-  Future<void> sendNotificationsWithinRadius(
-      String topic, double markerLat, double markerLng) async {
-    try {
-      if (location == null) {
-        throw 'Location not available';
-      }
+  // Future<void> sendNotificationsWithinRadius(
+  //     String topic, double markerLat, double markerLng) async {
+  //   try {
+  //     if (location == null) {
+  //       throw 'Location not available';
+  //     }
 
-      // Calculate distance between current location and marker
-      double distanceInMeters = Geolocator.distanceBetween(
-        location!.latitude,
-        location!.longitude,
-        markerLat,
-        markerLng,
-      );
-      double distanceInKm = distanceInMeters / 1000;
+  //     // Calculate distance between current location and marker
+  //     double distanceInMeters = Geolocator.distanceBetween(
+  //       location!.latitude,
+  //       location!.longitude,
+  //       markerLat,
+  //       markerLng,
+  //     );
+  //     double distanceInKm = distanceInMeters / 1000;
 
-      if (distanceInKm <= 1) {
-        // Send notification
-        await sendNotification(topic);
-      }
-    } catch (e) {
-      print('Error sending notification: $e');
-    }
+  //     if (distanceInKm <= 1) {
+  //       // Send notification
+  //       await sendNotification(topic);
+  //     }
+  //   } catch (e) {
+  //     print('Error sending notification: $e');
+  //   }
+  // }
+
+  // Future<void> sendNotification(String topic) async {
+  //   try {
+  //     final url = Uri.parse('https://fcm.googleapis.com/fcm/send');
+  //     final serverKey =
+  //         'AAAAXfsHsVE:APA91bGiEJp1y1T2h9VEHr0y-u0LoGIty04jFaHea0xtvwsUXOutSi2F5lRkwhEQmHV4MmQs2kEtoHhkxX1LDAeSGeHuk5ZzYZ5Gkec2oTUmqaktuf3Gv3Q4KuMsjpG2M6w76VHaxxbh'; // Replace with your FCM server key
+  //     final headers = {
+  //       'Content-Type': 'application/json',
+  //       'Authorization': 'key=$serverKey',
+  //     };
+  //     final body = {
+  //       'notification': {
+  //         'title': 'Someone nearby needs help.',
+  //         'body': _model.textController1.text,
+  //       },
+  //       'priority': 'high',
+  //       'to': '/topics/$topic', // Specify the topic here
+  //     };
+  //     final response = await http.post(
+  //       url,
+  //       headers: headers,
+  //       body: jsonEncode(body),
+  //     );
+  //     if (response.statusCode == 200) {
+  //       print('Notification sent successfully to topic: $topic');
+  //     } else {
+  //       print(
+  //           'Failed to send notification to topic: $topic. Error: ${response.reasonPhrase}');
+  //     }
+  //   } catch (e) {
+  //     print('Error sending notification: $e');
+  //   }
+  // }
+  Future<String> getAccessToken() async {
+    final serviceAccountCredentials = ServiceAccountCredentials.fromJson({
+      "type": "service_account",
+      "project_id": "kindmap-999d3",
+      "private_key_id": "17c31e6a632800b89dcc51ea5741db18908734d5",
+      "private_key":
+          "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCTRc9ik4ni99tP\nFc9JjUcZPdQ3/WeXbSZdh09Lbpjmq89G86qQpwv+KwojXK8w5wgSuee07ps2Jyum\nTGDE8SQqx5rvT0RdhBRZ+lJiKFHsORB4xhzFwnhYNDYHI1ZQYNArvprBQ70kzucd\nibpV2/5/R7codjFLji4+JKMuPL3NF8JjZ7rEBrAxgWQlgwId4ab0P1y3Db0gmJLE\nTXwMqh2AlPHi8CK0n+jrEQ/0hOAwSd/PXAczFGHqDX+zdMQ9OJ+/7457YHM2mx7h\nJJDDb1Nvv9NbgGQA4gJ3WdbPeJPJvFroc2IWYjTKlyq0KY4oOCRjW34MYTUA4ggc\ng0CqzvqvAgMBAAECggEAArlUVCTuycwUJNrTlNna76G/9px7xDETIsC5cTzlnBf1\nGKwHbW8sNB0fWLcX+xtoKqcrR3fwVqiT0JrHYW0lDPDYh0ZGjpo+TB8VU1Zz6XQc\nFLfJWXO23nQcxGCyxyrQlDaNeIykXMrCOz7MZlZ61xIrAOxquKneZheEYyW2zFUg\necAe+ESb7an/i0oDsyRAUTega+eLoqYehhNtXzZFnY4BFAJZu4X2zhi9jnKpPFSr\n889oIbHmAbZ/A+mYHZGYqOAvNH7PoB9t16W2Ewex8nk1Wbpki+emwwul49NfgjaJ\nmAYGyXu9a5uhZ2OOSTIrn1iJKR8JhlGA/OKo5Zn2IQKBgQDMwsXZL2u4EgGNfDAL\nMG0Ubrqe3X792GEJEe7O9GCky6YSWClYk7roe8ZAvQtzPU3v+jd4z4S69EpBILo0\nCl2852bMJn+GgEvSjIYvmvNRJWc4bS9KQhV8qbigFrNmaHa7VFEYka5/JZ2QUGDJ\n2wDw5pjGfN2rFEk1YnqU2W59oQKBgQC4IEcOqILaE3oXpdy8CttKcPluXVkzm56H\nKAyjmNZfgYSLjiKtbtpxceMm9mGXrpzXMoT6SV3xu9M2LhKW2hwPn9yw3sicrzAn\nmijx8yL/DT5ZVYDOo4xtBFj26YAC+4m9Bu02XkJj3/UPWdTfmQr1o8w4JLbl5Mbe\nZp31pQ52TwKBgDuwCzxkNmJR3WIA8YBRfXqXTI9CweH9UUvzjkmFsyZWtvJiAKtx\nZOqgKgp1EQFmvXFW3xS4aViWHY8emyjQXMLUMYMRNdtfSrr1e6gk4wikfpJUQZTD\n7r+IOelwtJsFmJbC3WDsFpG5xVRsGcq9rGiMz7wMahGUuEJ3koQRXcQBAoGAVmxI\nDfhIWuWzc/AVGGocHefDG+tS2CdeFGBW9l7hmDhppztSyYbznzXugbY5foGl+lgr\nFHNlVfZsH80mSoobi7XkV1xqWyjbeGsidtZBgeeMcU/xwov/eJgGzfYxcLTyJLhg\nlRlPHiPbmZX3le/2te9pBp0s/+EO+wq9b7RGgn8CgYBYL4ifTzjLYmpkX2tpI68L\nSK6JA8XbglHdllKq1QuGmX7XkpEGPj/v6lo/KHDMhTj6ysM4rn9/0IqXKlVHbC+W\n02NuRbrpOIRg9kKcuOKInGTae7GCmKbC3/d2BsMHkFs3831GxUp9VVckhuQDZXiG\n4/kYhaBJwRaqpEjWSwpPww==\n-----END PRIVATE KEY-----\n",
+      "client_email":
+          "firebase-adminsdk-ga6hv@kindmap-999d3.iam.gserviceaccount.com",
+      "client_id": "109937053761461466716",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url":
+          "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url":
+          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ga6hv%40kindmap-999d3.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+    });
+
+    final scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
+
+    var client =
+        await clientViaServiceAccount(serviceAccountCredentials, scopes);
+    return client.credentials.accessToken.data;
   }
 
   Future<void> sendNotification(String topic) async {
-    try {
-      final url = Uri.parse('https://fcm.googleapis.com/fcm/send');
-      final serverKey =
-          'AAAAXfsHsVE:APA91bGiEJp1y1T2h9VEHr0y-u0LoGIty04jFaHea0xtvwsUXOutSi2F5lRkwhEQmHV4MmQs2kEtoHhkxX1LDAeSGeHuk5ZzYZ5Gkec2oTUmqaktuf3Gv3Q4KuMsjpG2M6w76VHaxxbh'; // Replace with your FCM server key
-      final headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'key=$serverKey',
-      };
-      final body = {
-        'notification': {
-          'title': 'Someone nearby needs help.',
-          'body': _model.textController1.text,
-        },
-        'priority': 'high',
-        'to': '/topics/$topic', // Specify the topic here
-      };
-      final response = await http.post(
-        url,
-        headers: headers,
-        body: jsonEncode(body),
-      );
-      if (response.statusCode == 200) {
-        print('Notification sent successfully to topic: $topic');
-      } else {
-        print(
-            'Failed to send notification to topic: $topic. Error: ${response.reasonPhrase}');
-      }
-    } catch (e) {
-      print('Error sending notification: $e');
+    final fburl = Uri.parse(
+        'https://fcm.googleapis.com/v1/projects/kindmap-999d3/messages:send');
+    final accessToken = await getAccessToken();
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $accessToken',
+    };
+    final body = {
+      'notification': {
+        'title': 'KindMap',
+        'body': 'Someone nearby needs help.',
+      },
+      'priority': 'high',
+      'topic': '/topics/$topic',
+    };
+    final response = await http.post(
+      fburl,
+      headers: headers,
+      body: jsonEncode(body),
+    );
+    if (response.statusCode == 200) {
+      print('Notification sent successfully to topic: $topic');
+    } else {
+      print(
+          'Failed to send notification to topic: $topic. Error: ${response.reasonPhrase}');
     }
   }
 
@@ -802,8 +857,8 @@ class _PinPageState extends State<PinPage> with TickerProviderStateMixin {
                       onPressed: () async {
                         await uploadImage();
                         await sendNotification('need_help');
-                        await sendNotificationsWithinRadius('need_help',
-                            location!.latitude, location!.longitude);
+                        // await sendNotificationsWithinRadius('need_help',
+                        //     location!.latitude, location!.longitude);
                         Navigator.of(context).push(MaterialPageRoute(
                             builder: (context) =>
                                 PinConfirmation(docName: docName!)));
